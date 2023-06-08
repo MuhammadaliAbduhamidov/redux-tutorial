471719da0bf069695a140f9815f611110a234aba
MuhammadaliAbduhamidov <muhammadaliabduhamidov01@gmail.com>
2023-06-08 16:08:27 +0500

    store

--- src/Counter/Counter.js
+++ src/Counter/Counter.js
@@ -3,23 +3,33 @@ import { useDispatch, useSelector } from "react-redux";
 
 function Counter() {
   const selecter = useSelector((state) => state.counter);
+  const showCounter = useSelector((state) => state.showCounter);
 
   const dispatch = useDispatch();
   console.log(dispatch);
   function decr() {
     dispatch({ type: "decrement" });
   }
-  function incr() {
-    dispatch({ type: "increment" });
+  function incr(amount) {
+    dispatch({ type: "increment", amount: amount });
+  }
+  function ToggleCounter() {
+    dispatch({ type: "toggle" });
   }
   return (
-    <div>
+    <div className="wrapper">
       <h1>Redux counter</h1>
-      <div>Counter value: {selecter}</div>
-      <div>
-        <button onClick={decr}>Decrement</button>
-        <button onClick={incr}>Increment</button>
+      <div className="counter">
+        Counter value: {showCounter ? selecter : ""}
       </div>
+      <div className="btns">
+        <button onClick={decr}>Decrement</button>
+        <button onClick={() => incr(5)}>Increment5</button>
+        <button onClick={() => incr(1)}>Increment</button>
+      </div>
+      <button onClick={ToggleCounter} style={{ marginTop: "10px" }}>
+        ToggleCounter
+      </button>
     </div>
   );
 }
